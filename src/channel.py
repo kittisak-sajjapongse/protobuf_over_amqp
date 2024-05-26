46fc6b08c56efa0fc93098023a0f9b905d5baf9f
Kittisak Sajjapongse <kittisak.sajjapongse@gmail.com>
2024-05-26 15:43:29 -0400

    Enabled publish delivery confirmation + Handled disconnect and reconnect

--- src/channel.py
+++ src/channel.py
@@ -1,3 +1,4 @@
+import time
 from abc import abstractmethod
 
 import pika
@@ -7,18 +8,43 @@ from messages_pb2 import Response
 
 class Channel:
     def __init__(self, host: str, name: str):
-        self.name = name
-        self.connection_ = pika.BlockingConnection(
-            pika.ConnectionParameters(host=host))
+        self.name_ = name
+        self.host_ = host
+        self._connect()
+        self._configure_channel()
+
+    def _connect(self):
+        connected = False
+        while not connected:
+            try:
+                self.connection_ = pika.BlockingConnection(
+                    pika.ConnectionParameters(host=self.host_))
+                connected = True
+                print("Connected")
+            except pika.exceptions.AMQPConnectionError:
+                # Wait for 5 secs before trying to connect again
+                print("Attempt to connect to RabbitMQ. Reconnecting in 5 seconds...")
+                time.sleep(5)
         self.channel_ = self.connection_.channel()
-        self.channel_.queue_declare(queue=self.name, durable=True)
+
+    def _configure_channel(self):
+        # Enable Publisher Confirms
+        self.channel_.confirm_delivery()
+        # Declare a durable queue
+        self.channel_.queue_declare(queue=self.name_, durable=True)
 
     def send_response(self, response: Response):
-        self.channel_.basic_publish(
-            exchange='', routing_key=self.name, body=response.SerializeToString(),
-            properties=pika.BasicProperties(
-                delivery_mode=pika.DeliveryMode.Persistent,  # make message persistent
-            ))
+        try:
+            self.channel_.basic_publish(
+                exchange='', routing_key=self.name_, body=response.SerializeToString(),
+                properties=pika.BasicProperties(
+                    delivery_mode=pika.DeliveryMode.Persistent,  # make message persistent
+                ))
+        except pika.exceptions.UnroutableError:
+            # When the sender doesn't receive confirmation from RabbitMQ.
+            # Example: The sender got disconnected in the middle of transmitting a message.
+            # We can implement a mechnism to stash and re-send the message here.
+            print("Unable to send message to RabbmitMQ.")
 
     @abstractmethod
     def handle_response(self, ch, method, properties, body):
@@ -32,6 +58,18 @@ class Channel:
             # Call the callback to handle response
             self.handle_response(response)
 
-        self.channel_.basic_consume(
-            queue=self.name, on_message_callback=handle_receive, auto_ack=True)
-        self.channel_.start_consuming()
+        while True:
+            try:
+                self.channel_.basic_consume(
+                    queue=self.name_, on_message_callback=handle_receive, auto_ack=True)
+                print("Start consuming.")
+                self.channel_.start_consuming()
+            except pika.exceptions.ConnectionClosedByBroker:
+                print("Connection to RabbitMQ closed.")
+                self._connect()
+            except pika.exceptions.StreamLostError:
+                print("Stream lost error occurred.")
+                self._connect()
+            except Exception as e:
+                print("Unexpected error occurred.")
+                self._connect()
